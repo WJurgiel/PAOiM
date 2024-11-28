cd1dfea8c91ffc4639cafe6c6c87eb1aacf5e8fe
WJurgiel <wjurgiel@student.agh.edu.pl>
2024-11-28 14:35:10 +0100

    adding teachers works

--- lab3/TeachersManagement/src/main/java/com/example/teachers/AddTeacherFormController.java
+++ lab3/TeachersManagement/src/main/java/com/example/teachers/AddTeacherFormController.java
@@ -20,8 +20,8 @@ public class AddTeacherFormController extends SceneChanger implements Initializa
     @FXML private TextField nameTF;
     @FXML private TextField surnameTF;
     @FXML private TextField birthTF;
+    @FXML private TextField salaryTF;
     @FXML private ChoiceBox<String> condCB;
-    @FXML private ChoiceBox<String> classCB;
     Teacher t1 = new Teacher("Karol", "Laudański", TeacherConditions.ABSENT, 1080, 3000 );
     Teacher t2 = new Teacher("Melania", "Laudańska", TeacherConditions.DELEGATION, 1850, 4000 );
     @FXML
@@ -29,14 +29,28 @@ public class AddTeacherFormController extends SceneChanger implements Initializa
 
         ObservableList<ClassTeacher> group = SharedData.getClassesInGroup().get(ClassPreviewBoardController.currentGroup);
         if(group != null && !group.isEmpty()){
-            ClassTeacher chosenClass = group.get(ClassPreviewBoardController.currentClassID); // tryout for first class only
+            ClassTeacher chosenClass = group.get(ClassPreviewBoardController.currentClassID);
             if(chosenClass.getTeachers().size() >= chosenClass.maxTeachers){
                 displayNoMoreRoomDialog();
                 return;
             }
-            chosenClass.addTeacher(t1);
+
+            if(isDataWrong()){
+                displayWrongTeacher();
+            }
+            else{
+                String name = nameTF.getText();
+                String surname = surnameTF.getText();
+                Integer birtYear = Integer.valueOf(birthTF.getText());
+                Integer salary = Integer.valueOf(salaryTF.getText());
+                TeacherConditions cond = TeacherConditions.valueOf(condCB.getValue());
+                Teacher teacherToBeAdded = new Teacher(name,surname,cond,birtYear,salary);
+                chosenClass.addTeacher(teacherToBeAdded);
+                changeScene(event, "class-preview-board.fxml");
+            }
+
         }
-        changeScene(event, "class-preview-board.fxml");
+
 
     }
     private void displayNoMoreRoomDialog(){
@@ -45,11 +59,23 @@ public class AddTeacherFormController extends SceneChanger implements Initializa
         alert.setHeaderText("There's no more room left in this class");
         alert.showAndWait();
     }
+    private void displayWrongTeacher(){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("Data is incorrect");
+        alert.showAndWait();
+    }
+    private boolean isDataWrong(){
+        boolean nameWrong = nameTF.getText().trim().isEmpty() || nameTF.getText().matches(".*\\d.*");
+        boolean surnameWrong = (surnameTF.getText().trim().isEmpty() || surnameTF.getText().matches(".*\\d.*"));
+        boolean birthWrong = (birthTF.getText().trim().isEmpty() || birthTF.getText().matches(".*\\D.*") || Integer.parseInt(birthTF.getText()) > 2024);
+        boolean salaryWrong = salaryTF.getText().trim().isEmpty() || !salaryTF.getText().matches("\\d+");
+        return nameWrong || surnameWrong || birthWrong || salaryWrong;
 
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-//        classCB.setValue(SharedData.getClassesInGroup().get(ClassPreviewBoardController.currentGroup).get(ClassPreviewBoardController.currentClassID).getTheClassName());
-        condCB.setItems(FXCollections.observableArrayList(Arrays.stream(TeacherConditions.values()).map(Enum::name).toList()));
-//        classCB.setItems(FXCollections.observableArrayList(SharedData.getClassesInGroup().keySet()));
+         condCB.setItems(FXCollections.observableArrayList(Arrays.stream(TeacherConditions.values()).map(Enum::name).toList()));
+         condCB.setValue(TeacherConditions.PRESENT.name());
     }
 }
